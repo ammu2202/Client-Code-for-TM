65ec5ce5bfdcde833b61f64c293d086f9b442d3e
ammu2202 <ananyaprasadpillai@gmail.com>
2022-12-13 12:51:37 +0530

    client code for TM dapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,160 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { contractAddress } from "./config";
+import TaskAbi from "./TaskManager.json";
+import { useState, useEffect } from "react";
 
+import { ethers } from "ethers";
 function App() {
+  //for connect wallet
+  const [isUserLoggedIn, setUserLoggedIn] = useState(false);
+  const [currentsAccount, setCurrentAccount] = useState("");
+
+  //for add tasks
+  const [input, setInput] = useState(" ");
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    connectWallet();
+    getAllTasks();
+  }, []);
+
+  // Calls Metamask to connect wallet on clicking Connect Wallet button
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("metamask not detected");
+        return;
+      } else {
+        const accounts = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        console.log("Found account", accounts[0]);
+        setUserLoggedIn(true);
+        setCurrentAccount(accounts[0]);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getAllTasks = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const TaskContract = new ethers.Contract(
+          contractAddress,
+          TaskAbi.abi,
+          signer
+        );
+
+        let allTasks = await TaskContract.getTasks();
+        let filtered = allTasks.filter((task) => task.isDeleted !== true);
+        setTasks(filtered);
+      } else {
+        console.log("ethereum object does not exist");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addTasks = async (e) => {
+    e.preventDefault(); //avoid refresh
+    let task = {
+      taskText: input,
+      isDeleted: false,
+    };
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const TaskContract = new ethers.Contract(
+          contractAddress,
+          TaskAbi.abi,
+          signer
+        );
+
+        TaskContract.addTask(task.taskText, task.isDeleted)
+          .then((res) => {
+            setTasks([...tasks, task]);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        setInput(" ");
+      } else {
+        console.log("ethereum object does not exist");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteTask = (key) => async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const TaskContract = new ethers.Contract(
+          contractAddress,
+          TaskAbi.abi,
+          signer
+        );
+
+        const deleteTaskTx = await TaskContract.deleteTask(key, true);
+        console.log("Successfully deleted: ", deleteTaskTx);
+
+        const allTasks = await TaskContract.getTasks();
+        let filtered = allTasks.filter((task) => task.isDeleted !== true);
+        setTasks(filtered);
+      } else {
+        console.log("ethereum object does not exist");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isUserLoggedIn && (
+        <div className="connectWalletButton">
+          <button onClick={connectWallet}>Connect</button>
+        </div>
+      )}
+
+      {isUserLoggedIn && (
+        <div className="inputform">
+          <form>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button onClick={addTasks}>Submit</button>
+          </form>
+        </div>
+      )}
+
+      <div className="alltasks">
+        {tasks.map((task) => {
+          return (
+            <div key={task.id}>
+              <h1>
+                {task.taskText}{" "}
+                <span>
+                  <button onClick={deleteTask(task.id)}>Delete</button>
+                </span>
+              </h1>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const contractAddress = '0x24230Dc8E6C9F8cF0B9e370F2c31b5b9FA7ce5f7'
+// Deploying contracts with the account: 0x01d72d5f4f0dC8943E1D9ABC28F544329BacDdF2
+// Account balance: 106813878875526147
+// Token address: 0x24230Dc8E6C9F8cF0B9e370F2c31b5b9FA7ce5f7
